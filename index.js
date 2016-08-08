6ed26da9df30b43bb719b901b8ad59e3477a47fc
James Reichard <jpqreichard@gmail.com>
2016-08-08 09:50:59 -0400

    downloads repo

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var request = require("request");
+var exec    = require("child_process").execSync;
+var sys     = require("sys");
+
 
 function url(){
-  return "https://api.github.com/orgs/ga-wdi-exercises/repos?access_token=" + process.env.github_access_token;
+  return "https://api.github.com/orgs/ga-wdi-exercises/repos?access_token=" + process.env.github_access_token + "&limit=1";
 }
 
 function repos (callback){
   request( url(), { headers: { "User-Agent": "migrate_to_gitlab" } }, function (err, res, body){
-    callback( JSON.parse(body) );
+    callback( JSON.parse(body).map(function (repo) {
+      return repo.clone_url;
+    }) );
   } )
 }
 
 repos( function(repositories){
-  console.log(repositories.length);
+  exec("rm -rf repos/*")
+  repositories.forEach(function (repo){
+    console.log( exec("cd repos && git clone --depth=1 " + repo + " && cd ..") );
+  })
 })
